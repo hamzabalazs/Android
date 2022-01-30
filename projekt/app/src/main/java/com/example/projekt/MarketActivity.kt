d02402c4ca3c36367cb28977d447f2acc02f5a3f
hamzabalazs <hamza.z.balazs@student.ms.sapientia.ro>
2022-01-30 17:26:53 +0200

    Implemented MarketActivity ( design )

---
+++ projekt/app/src/main/java/com/example/projekt/MarketActivity.kt
@@ -0,0 +1,25 @@
+package com.example.projekt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MarketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market)
+    }
+
+    fun goToTimeline(view: View){
+        startActivity(Intent(this,TimeLineActivity::class.java))
+    }
+
+    fun goToFares(view: View){
+        startActivity(Intent(this,FaresActivity::class.java))
+    }
+
+    fun goBackToMain(view: View){
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+}
