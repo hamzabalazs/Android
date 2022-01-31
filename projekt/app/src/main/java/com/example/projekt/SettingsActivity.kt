b8e8d513e191ee5a45df7d3cf18255888c0c95f9
hamzabalazs <hamza.z.balazs@student.ms.sapientia.ro>
2022-01-31 17:39:29 +0200

    Added validation function to SettingsActivity

--- projekt/app/src/main/java/com/example/projekt/SettingsActivity.kt
+++ projekt/app/src/main/java/com/example/projekt/SettingsActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
     }
+
+    val settingsEmail = findViewById<EditText>(R.id.editTextSettingsEmail)
+    val settingsUsername = findViewById<EditText>(R.id.editTextSettingsUsername)
+    val settingsPhonenumber = findViewById<EditText>(R.id.editTextSettingsPhonenumber)
+    val settingsPassword = findViewById<EditText>(R.id.editTextSettingsPassword)
+    val settingsButton = findViewById<Button>(R.id.PublishSettingsButton)
 
     fun goToTimeline(view: View){
         startActivity(Intent(this,TimeLineActivity::class.java))
@@ -26,4 +34,46 @@ class SettingsActivity : AppCompatActivity() {
     fun goToFares(view: View){
         startActivity(Intent(this,FaresActivity::class.java))
     }
+    fun settingsValidation(email: EditText, username: EditText, phonenumber: EditText, password: EditText){
+        val emailstring = email.text.toString()
+        var isError = false
+        if (email.text.isNullOrEmpty()){
+            email.error = "Email is empty!"
+            isError = true
+        }
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailstring).matches()){
+            email.error = "Email format is wrong!"
+            isError = true
+
+        }
+        if(password.length() < 8){
+            password.error = "Password has to be 8 characters or more!"
+            isError = true
+        }
+        if(password.text.isNullOrEmpty()){
+            password.error = "Password is empty!"
+            isError = true
+        }
+
+        if(username.text.isNullOrEmpty()){
+            username.error = "Username is empty!"
+            isError = true
+        }
+
+        if(phonenumber.length() != 10){
+            phonenumber.error = "Incorrect Phonenumber! Phonenumber must be 10 digits!"
+            isError = true
+        }
+
+        if(phonenumber.text.isNullOrEmpty()){
+            phonenumber.error = "PhoneNumber is empty!"
+            isError = true
+        }
+
+        if(!isError){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+
 }
